71f7e659d52f541c31e18af9932af49265afb494
ItsNeil17 <neil@willofsteel.me>
2025-11-20 03:15:00 +0530

    feat: add site and bot commands
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- cogs/keywords.py
+++ cogs/keywords.py
@@ -5,8 +5,7 @@ class Keywords(commands.Cog):
     def __init__(self, bot):
         self.bot: commands.Bot = bot
     
-    @commands.command()
-    async def languages(self, ctx: commands.Context):
+    async def get_replied_message(self, ctx: commands.Context) -> discord.Message:
         if ctx.message.reference:
             replied_message = ctx.message.reference.resolved
             if replied_message is None:
@@ -15,6 +14,11 @@ class Keywords(commands.Cog):
                 )
         else:
             replied_message = ctx.message
+        return replied_message
+
+    @commands.command()
+    async def languages(self, ctx: commands.Context):
+        replied_message = await self.get_replied_message(ctx)
         
         e = discord.Embed(description="Hytale will support the following languages:\n\n- Server Side (Plugins): Java\n- Client Side (Modding): C#\n\n-# *This information is subject to change as Hytale is still in development. It is not confirmed.*")
         e.set_footer(text="Hytale Modding", icon_url='https://img.willofsteel.me/u/p2SdbC.png')
@@ -22,18 +26,23 @@ class Keywords(commands.Cog):
     
     @commands.command()
     async def security(self, ctx: commands.Context):
-        if ctx.message.reference:
-            replied_message = ctx.message.reference.resolved
-            if replied_message is None:
-                replied_message = await ctx.channel.fetch_message(
-                    ctx.message.reference.message_id
-                )
-        else:
-            replied_message = ctx.message
+        replied_message = await self.get_replied_message(ctx)
         
         e = discord.Embed(description="Hypixel Studios have a bug bounty program to help improve the security of Hytale. \n\n### [Click here or the link above to visit](https://hytale.com/security)")
         e.set_footer(text="Hytale Modding", icon_url='https://img.willofsteel.me/u/p2SdbC.png')
         await replied_message.reply(content="https://hytale.com/security", embed=e)
+
+    @commands.command()
+    async def site(self, ctx: commands.Context):
+        replied_message = await self.get_replied_message(ctx)
+        
+        await replied_message.reply("https://github.com/HytaleModding/site")
+
+    @commands.command()
+    async def bot(self, ctx: commands.Context):
+        replied_message = await self.get_replied_message(ctx)
+        
+        await replied_message.reply("https://github.com/HytaleModding/robot")
 
 async def setup(bot):
     await bot.add_cog(Keywords(bot))
