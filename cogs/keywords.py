64234837cf48251c00bebbf956f92d4a334c3c56
Wesley H <drakonkinstmc@gmail.com>
2025-11-20 08:20:13 -0800

    Update some wording

--- cogs/keywords.py
+++ cogs/keywords.py
@@ -20,7 +20,7 @@ class Keywords(commands.Cog):
     async def languages(self, ctx: commands.Context):
         replied_message = await self.get_replied_message(ctx)
         
-        e = discord.Embed(description="Hytale will support the following languages:\n\n- Server Side (Plugins): Java\n- Client Side (Modding): C#\n- UI Engine: Noesis Engine\n\n-# *This information is subject to change as Hytale is still in development. It is not confirmed.*")
+        e = discord.Embed(description="Hytale will support the following languages:\n\n- The server is written in Java (currently Java 25).\n- The client is written in C#.\n- It is unknown how the client will be moddable, if at all. Servers will be fully moddable and will have a **handshake process** where textures and other resources are sent from the server to the client the first time you join.\n- In addition, modders will be able to use **Noesis Engine** to create custom UI, which supports C++ and C# (assumedly C# will be used).\n\n-# *This information is subject to change as Hytale is still in development. It is not confirmed.*")
         e.set_footer(text="Hytale Modding", icon_url='https://img.willofsteel.me/u/p2SdbC.png')
         await replied_message.reply(embed=e)
     
@@ -28,7 +28,7 @@ class Keywords(commands.Cog):
     async def security(self, ctx: commands.Context):
         replied_message = await self.get_replied_message(ctx)
         
-        e = discord.Embed(description="Hypixel Studios have a bug bounty program to help improve the security of Hytale. \n\n### [Click here or the link above to visit](https://hytale.com/security)")
+        e = discord.Embed(description="Hypixel Studios will have a bug bounty program to help improve the security of Hytale. \n\n### [Click here or the link above to visit](https://hytale.com/security)")
         e.set_footer(text="Hytale Modding", icon_url='https://img.willofsteel.me/u/p2SdbC.png')
         await replied_message.reply(content="https://hytale.com/security", embed=e)
 
@@ -59,7 +59,7 @@ class Keywords(commands.Cog):
     async def threadpin(self, ctx: commands.Context):
         replied_message = await self.get_replied_message(ctx)
 
-        e = discord.Embed(description="Right click any message in a thread you own and select 'Apps' -> 'Pin Message' to pin that message to the thread. Only the thread owner can pin messages in their thread.")
+        e = discord.Embed(description="Right-click any message in a thread you own and select 'Apps' -> 'Pin Message' to pin that message to the thread. Only the thread owner can pin messages in their thread.")
         e.set_footer(text="Hytale Modding", icon_url='https://img.willofsteel.me/u/p2SdbC.png')
         await replied_message.reply(embed=e)
 
